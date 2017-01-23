f068c66d830732312a443c12336b1c0d7b2d93e9
ItGeneral <799178171@qq.com>
2017-01-23 16:44:41 +0800

    add routing config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import {appRouting} from "./app.routing";
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    appRouting
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,18 @@
+/**
+ * @Author SongJiuHua
+ * Created on 2017/1/23
+ * @Description 路由配置
+ */
+
+import {ModuleWithProviders} from "@angular/core";
+import {RouterModule, Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+const appRoutes: Routes = [
+  {path:'', redirectTo:'', pathMatch:'full'},
+  {path:'home', component: HomeComponent},
+  {path:'about', component: AboutComponent},
+];
+
+export const appRouting:ModuleWithProviders  = RouterModule.forRoot(appRoutes);
