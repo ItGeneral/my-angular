ac5fac53afefb7ca729d04d8c05af8fa990200bc
ItGeneral <799178171@qq.com>
2017-02-23 19:41:58 +0800

    edit httpservice

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,19 +8,28 @@ import {HttpService} from "../http.service";
 })
 export class HomeComponent extends HttpService implements OnInit {
 
-  private obj : Object[];
+  obj : Object[];
+
+  p:Promise<Object>;
 
   //生命周期钩子  Angular提供了一些接口，用来介入组件生命周期的几个关键时间点：刚创建时、每次变化时，以及最终被销毁时。
   //初始化加载时执行this.get()方法获取数据 http://localhost:8080/user
   ngOnInit(): void {
-    this.getData();
+    this.testHttp();
+    console.log("---3-----" + this.obj);
   }
 
-  getData(){
+  testHttp(){
+    //subscribe是异步 then()是同步
     this.get("http://localhost:8080/user", null).subscribe(function (data) {
       this.obj = data;
-      console.log(this.obj)
-    })
+      console.log("---1-----" + this.obj);
+    });
+    console.log("---2-----" + this.obj);
+
+    this.p = this.gets("http://localhost:8080/user", null).then(response => response as Object[])
+    console.log(this.p)
   }
+
 
 }
