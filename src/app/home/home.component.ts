b5190b9e8f3244009f4e43574c5e87f364c7671e
ItGeneral <799178171@qq.com>
2017-03-13 19:09:49 +0800

    Follow up the project

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpService} from "../service/http.service";
+import {NgModel} from "@angular/forms";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers:[NgModel]
 })
 export class HomeComponent implements OnInit {
 
   constructor(public httpService : HttpService){}
 
   public obj : Array<Object>;
+
+  public target : string;
 
   //生命周期钩子  Angular提供了一些接口，用来介入组件生命周期的几个关键时间点：刚创建时、每次变化时，以及最终被销毁时。
   //初始化加载时执行this.get()方法获取数据 http://localhost:8080/user
@@ -24,6 +28,10 @@ export class HomeComponent implements OnInit {
       this.obj = data;
       //console.log(this.obj)
     });
+  }
+
+  search(){
+    console.log(this.target);
   }
 
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,14 @@ import {HomeComponent} from "./home.component";
 import {CommonModule} from "@angular/common";
 import {RouterModule} from "@angular/router";
 import {homeRoutes} from "./home.route";
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(homeRoutes)
+    RouterModule.forChild(homeRoutes),
+    FormsModule //必须导入该module，才能进行双向绑定
   ],
   declarations: [ //声明本模块中拥有的视图类
     HomeComponent,
