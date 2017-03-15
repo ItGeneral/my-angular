400067ed9a9fd1e9b9ce3ed6778125a507fec61a
ItGeneral <799178171@qq.com>
2017-03-15 18:39:44 +0800

    do nothing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,7 +19,7 @@ export class HomeComponent implements OnInit {
   //生命周期钩子  Angular提供了一些接口，用来介入组件生命周期的几个关键时间点：刚创建时、每次变化时，以及最终被销毁时。
   //初始化加载时执行this.get()方法获取数据 http://localhost:8080/user
   ngOnInit(): void {
-    this.testHttp();
+    //this.testHttp();
   }
 
   testHttp(){
